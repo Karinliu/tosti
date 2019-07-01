3c45030bd5cb70efdb6ea315d2c47d1e86a22b4e
karinliu <karin-liu@hotmail.com>
2019-07-01 18:44:59 +0200

    update names

--- src/js/index.js
+++ src/js/index.js
@@ -29,83 +29,67 @@
         btnJs.addEventListener("click", copy);
     }
 
-    // If querySelectorAll is supported
-    // if (document.querySelectorAll) {
-        console.log('querySelectorAll true')
-        // var fieldset = Array.from(document.querySelectorAll("fieldset"));
-        var fieldset = checkQuerySelectorAll("fieldset");
-
-        for (var i = 0; i < fieldset.length; i++) {
-            var inputs = fieldset[i].getElementsByTagName("input");
-
-            for (var c = 0; c < inputs.length; c++) {
-                // if addEventlistener is supported
-                if (document.addEventListener) {
-                    console.log('addevent true')
-                    inputs[c].addEventListener("keydown", inputChecked)
+    var fieldset = checkQuerySelectorAll("fieldset");
+    for (var i = 0; i < fieldset.length; i++) {
+        var inputs = fieldset[i].getElementsByTagName("input");
+        for (var c = 0; c < inputs.length; c++) {
+            // if addEventlistener is supported
+            if (document.addEventListener) {
+                console.log('addevent true')
+                inputs[c].addEventListener("keydown", inputChecked)
+            } else {
+                var btn = document.getElementById("node-btn");
+                var btnJs = document.getElementById("js-btn");
+                btnJs.className += " hidden"
+                var classes = btn.className.split(" ");
+                var i = classes.indexOf("hidden");
+                if (i >= 0) {
+                    btn.className = "form-btn node-btn "
                 } else {
-                    var btn = document.getElementById("node-btn");
-                    var btnJs = document.getElementById("js-btn");
-                    btnJs.className += " hidden"
-                    var classes = btn.className.split(" ");
-                    var i = classes.indexOf("hidden");
-                    if (i >= 0) {
-                        btn.className = "form-btn node-btn "
-                    } else {
-                        btn.classname = "form-btn node-btn hidden"
-                    }
-                }
-            }
-            var fieldinputs = fieldset[i].getElementsByTagName("label");
-            for (var b = 0; b < fieldinputs.length; b++) {
-
-                // if addEventlistener is supported
-                if (document.addEventListener) {
-                    console.log('addevent true')
-                    fieldinputs[b].addEventListener("click", handleClick)
-                } else {
-                    console.log('do nothing')
-
-                    var btn = document.getElementById("node-btn");
-                    var btnJs = document.getElementById("js-btn");
-                    btnJs.className += " hidden"
-                    var classes = btn.className.split(" ");
-                    var i = classes.indexOf("hidden");
-                    if (i >= 0) {
-                        btn.className = "form-btn node-btn "
-                    } else {
-                        btn.classname = "form-btn node-btn hidden"
-                    }
+                    btn.classname = "form-btn node-btn hidden"
                 }
             }
         }
+        var fieldinputs = fieldset[i].getElementsByTagName("label");
+        for (var b = 0; b < fieldinputs.length; b++) {
+            if (document.addEventListener) {
+                console.log('addevent true')
+                fieldinputs[b].addEventListener("click", handleClick)
+            } else {
+                console.log('do nothing')
+                var btn = document.getElementById("node-btn");
+                var btnJs = document.getElementById("js-btn");
+                btnJs.className += " hidden"
+                var classes = btn.className.split(" ");
+                var i = classes.indexOf("hidden");
+                if (i >= 0) {
+                    btn.className = "form-btn node-btn "
+                } else {
+                    btn.classname = "form-btn node-btn hidden"
+                }
+            }
+        }
+    }
 
-    function inputChecked() { 
+    function inputChecked() {
         if (this.checked === true) {
             console.log("hij is goed")
-
-           var element = this;
-           // element.parentElement.className = ""
-           // element.parentElement.className += ("clickedKey");
-
-           // var parent = element.parentElement.parentElement
-
-           //  var fieldsetFirst = document.getElementsByTagName('fieldset')[0];
-
-           //  while (parent.firstChild) {
-           //      parent.removeChild(parent.firstChild);
-           //  }
-
-           //  parent.insertAdjacentElement("beforeend", element.parentElement);
-
+            var element = this;
+            // element.parentElement.className = ""
+            // element.parentElement.className += ("clickedKey");
+            // var parent = element.parentElement.parentElement
+            //  var fieldsetFirst = document.getElementsByTagName('fieldset')[0];
+            //  while (parent.firstChild) {
+            //      parent.removeChild(parent.firstChild);
+            //  }
+            //  parent.insertAdjacentElement("beforeend", element.parentElement);
             element.removeEventListener("keydown", inputChecked)
             var foo = element.parentElement;
             var list = document.getElementById("list");
             list.className = "list paper paper-list "
 
             document.getElementById("alltext").value += " " + foo.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ') || " " + foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
-        }
-        else {
+        } else {
             handleClick()
         }
     }
@@ -114,19 +98,15 @@
         var element = this;
         element.removeEventListener("click", handleClick)
         // element.className += "clicked";
-
         // var parent = element.parentElement.parentElement
-
         // while (parent.firstChild) {
         //     parent.removeChild(parent.firstChild);
         // }
         // parent.insertAdjacentElement("beforeend", element);
-
         // console.log(parent)
         var foo = element;
         var list = document.getElementById("list");
         list.className = "list paper paper-list "
-
         document.getElementById("alltext").value += " " + foo.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ') || " " + foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
     }
 
@@ -145,11 +125,11 @@
         }
     }
 
-    function checkQuerySelectorAll(query) {
+    function checkQuerySelectorAll(element) {
         if ('querySelectorAll' in document) {
-            return document.querySelectorAll(query);
+            return document.querySelectorAll(element);
         } else {
-            return document.getElementsByTagName(query);
+            return document.getElementsByTagName(element);
         }
     }
 
