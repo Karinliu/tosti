1075224348ede8ab5743bbeefd886f1d9a417062
karinliu <karin-liu@hotmail.com>
2019-07-01 13:42:23 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,165 +1,195 @@
 (function() {
-    function featureDetectionCheck(feature, where, type) {
-        return feature in where &&
-            type ?
-            typeof where[feature] === type :
-            true
-    }
+    // Check if querySelector is supported
+    if (!document.addEventListener) {
+        console.log("addEventlistener is not supported")
 
+        var btn = document.getElementById("node-btn");
+        var btnJs = document.getElementById("js-btn");
+        btnJs.className += " hidden"
+        var classes = btn.className.split(" ");
+        var i = classes.indexOf("hidden");
+        if (i >= 0) {
+            btn.className = "form-btn node-btn "
+        } else {
+            btn.classname = "form-btn node-btn hidden"
+        }
+    } else {
+        console.log('addEventlistener is supported')
+        var btn = document.getElementById("node-btn");
+        btn.className += " hidden"
+        var btnJs = document.getElementById("js-btn");
+        var classes = btnJs.className.split(" ");
+        var i = classes.indexOf("hidden");
+        if (i >= 0) {
+            btnJs.className = "form-btn js-btn "
+        } else {
+            btnJs.classname = "form-btn js-btn hidden"
+        }
 
-    function enableJavaScript() {
-        return featureDetectionCheck('classList', document.body) &&
-            featureDetectionCheck('Array', Array.prototype, 'function') &&
-            featureDetectionCheck('querySelectorAll', document.body, 'function') &&
-            featureDetectionCheck('querySelector', document.body) &&
-            featureDetectionCheck('getElementById', document.body)
-    }
-
-
-    if (enableJavaScript()) {
-        var fieldset = Array.from(document.querySelectorAll("fieldset"));
-        var btn = document.querySelector(".node-btn").classList.toggle("hidden");
-        var btnJs = document.querySelector(".js-btn");
-        btnJs.classList.toggle("hidden");
         btnJs.addEventListener("click", copy);
+    }
 
-        // var btnJs = document.getElementsByTagName("button")[1].classList.toggle("hidden");
-
-        // fieldset.forEach(field => {
-        //     field.querySelectorAll("input[type='radio'] + label").forEach(checkbox => {
-        //         checkbox.addEventListener("click", handleClick)
-        //     })
-        // })
+    // If querySelectorAll is supported
+    if (document.querySelectorAll) {
+        console.log('querySelectorAll true')
+        // var fieldset = Array.from(document.querySelectorAll("fieldset"));
+        var fieldset = document.querySelectorAll("fieldset");
 
         for (var i = 0; i < fieldset.length; i++) {
-            // console.log(fieldset[i]);
-            var inputs = fieldset[i].querySelectorAll("input[type='radio']");
-
-            console.log(inputs)
+            var inputs = fieldset[i].querySelectorAll("input[type='checkbox']");
 
             for (var c = 0; c < inputs.length; c++) {
-                // console.log(fieldinputs[b]);
-
-                inputs[c].addEventListener("keydown", inputChecked)
-            }
-
-
-            // for (a = 0; a < inputs.length; a++) {
-            //     if (inputs[i].type == 'radio') {
-            //         for (var c = 0; c < inputs.length; c++) {
-            //             // console.log(fieldinputs[b]);
-
-            //             inputs[c].addEventListener("keydown", inputChecked)
-            //         }
-            //     }
-            // }
-
-            var fieldinputs = fieldset[i].querySelectorAll("input[type='radio'] + label");
-            for (var b = 0; b < fieldinputs.length; b++) {
-                // console.log(fieldinputs[b]);
-
-                fieldinputs[b].addEventListener("click", handleClick)
-            }
-        }
-
-        function inputChecked() {
-            if (this.checked === true) {
-                console.log("hij is goed")
-
-                var element = this;
-                element.parentElement.classList = ""
-                element.parentElement.classList.add("clickedKey");
-
-                var parent = element.parentElement.parentElement
-
-                console.log(parent)
-                while (parent.firstChild) {
-                    parent.removeChild(parent.firstChild);
+                // if addEventlistener is supported
+                if (document.addEventListener) {
+                    console.log('addevent true')
+                    inputs[c].addEventListener("keydown", inputChecked)
+                } else {
+                    var btn = document.getElementById("node-btn");
+                    var btnJs = document.getElementById("js-btn");
+                    btnJs.className += " hidden"
+                    var classes = btn.className.split(" ");
+                    var i = classes.indexOf("hidden");
+                    if (i >= 0) {
+                        btn.className = "form-btn node-btn "
+                    } else {
+                        btn.classname = "form-btn node-btn hidden"
+                    }
                 }
-                parent.insertAdjacentElement("beforeend", element.parentElement);
+            }
+            var fieldinputs = fieldset[i].querySelectorAll("input[type='checkbox'] + label");
+            for (var b = 0; b < fieldinputs.length; b++) {
 
-                element.removeEventListener("keydown", inputChecked)
+                // if addEventlistener is supported
+                if (document.addEventListener) {
+                    console.log('addevent true')
+                    fieldinputs[b].addEventListener("click", handleClick)
+                } else {
+                    console.log('do nothing')
 
-                var foo = element.parentElement.cloneNode(true);
-
-                console.log(foo)
-
-                var list = document.querySelector(".list");
-                list.classList.remove("hidden");
-
-                document.getElementById("alltext").value += foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
-
-            } else {
-                handleClick()
+                    var btn = document.getElementById("node-btn");
+                    var btnJs = document.getElementById("js-btn");
+                    btnJs.className += " hidden"
+                    var classes = btn.className.split(" ");
+                    var i = classes.indexOf("hidden");
+                    if (i >= 0) {
+                        btn.className = "form-btn node-btn "
+                    } else {
+                        btn.classname = "form-btn node-btn hidden"
+                    }
+                }
             }
         }
+    } else {
+        console.log('querySelectorAll false')
+        var fieldset = document.getElementsByTagName('fieldset')
+        for (var i = 0; i < fieldset.length; i++) {
+            var inputs = fieldset[i].getElementsByTagName("input");
+            for (var c = 0; c < inputs.length; c++) {
 
-        function handleClick() {
-            var element = this;
-
-            element.removeEventListener("click", handleClick)
-            element.classList.add("clicked");
-
-            // console.log(element.removeEventListener("click", handleClick))
-            var parent = element.parentElement.parentElement
-
-
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
+                // if addEventlistener is supported
+                if (document.addEventListener) {
+                    console.log('addevent true')
+                    inputs[c].addEventListener("keydown", inputChecked)
+                } else {
+                    var btn = document.getElementById("node-btn");
+                    var btnJs = document.getElementById("js-btn");
+                    btnJs.className += " hidden"
+                    var classes = btn.className.split(" ");
+                    var i = classes.indexOf("hidden");
+                    if (i >= 0) {
+                        btn.className = "form-btn node-btn "
+                    } else {
+                        btn.classname = "form-btn node-btn hidden"
+                    }
+                }
             }
-            parent.insertAdjacentElement("beforeend", element);
+            var fieldinputs = fieldset[i].getElementsByTagName("label");
+            console.log(fieldinputs)
+            for (var b = 0; b < fieldinputs.length; b++) {
+                // if addEventlistener is supported
+                if (document.addEventListener) {
+                    console.log('addevent true')
+                    fieldinputs[b].addEventListener("click", handleClick)
+                } else {
+                    console.log('do nothing')
 
-            console.log(element)
-
-            var foo = element.cloneNode(true);
-
-            console.log(foo)
-
-            var list = document.querySelector(".list");
-            list.classList.remove("hidden");
-
-            document.getElementById("alltext").value += foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
-
-            // while (parent.firstChild) {
-            //     parent.removeChild(parent.firstChild);
-            // }
-
-            // listElements();
-        }
-
-        function copy() {
-            if (false == document.execCommand('copy')) {
-                var btn = document.querySelector(".node-btn").classList.add("hidden");
-                var btnJs = document.querySelector(".js-btn");
-                btnJs.classList.remove("hidden");
-            } else {
-
-                var copyText = document.getElementById("alltext");
-                console.log(copyText)
-                copyText.select();
-                document.execCommand("copy");
-                console.log("copy copy")
+                    var btn = document.getElementById("node-btn");
+                    var btnJs = document.getElementById("js-btn");
+                    btnJs.className += " hidden"
+                    var classes = btn.className.split(" ");
+                    var i = classes.indexOf("hidden");
+                    if (i >= 0) {
+                        btn.className = "form-btn node-btn "
+                    } else {
+                        btn.classname = "form-btn node-btn hidden"
+                    }
+                }
             }
         }
+    }
 
-        // function listElements() {
-        //     var list = document.querySelector(".list");
-        //     var clickedElement = document.querySelector(".clicked p");
-        //     list.classList.remove("hidden");
+    function inputChecked() { 
+        if (this.checked === true) {
+            console.log("hij is goed")
 
-        //     document.getElementById("alltext").value += clickedElement.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
+           var element = this;
+           // element.parentElement.className = ""
+           // element.parentElement.className += ("clickedKey");
 
-        //     // var listItem = document.createElement('p');
-        //     //     listItem.innerHTML = clickedElement.textContent || clickedElement.innerText;
-        //     //     list.appendChild(listItem);
+           // var parent = element.parentElement.parentElement
+
+           //  var fieldsetFirst = document.getElementsByTagName('fieldset')[0];
+
+           //  while (parent.firstChild) {
+           //      parent.removeChild(parent.firstChild);
+           //  }
+
+           //  parent.insertAdjacentElement("beforeend", element.parentElement);
+
+            element.removeEventListener("keydown", inputChecked)
+            var foo = element.parentElement;
+            var list = document.getElementById("list");
+            list.className = "list paper paper-list "
+
+            document.getElementById("alltext").value += " " + foo.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ') || " " + foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
+        }
+        else {
+            handleClick()
+        }
+    }
+
+    function handleClick() {
+        var element = this;
+        element.removeEventListener("click", handleClick)
+        // element.className += "clicked";
+
+        // var parent = element.parentElement.parentElement
+
+        // while (parent.firstChild) {
+        //     parent.removeChild(parent.firstChild);
         // }
+        // parent.insertAdjacentElement("beforeend", element);
 
-        // function copy(){
-        //     var copyText = document.getElementById("alltext");
-        //     console.log(copyText)
-        //     copyText.select();
-        //     document.execCommand("copy");
-        //  console.log("copy copy")
-        // }
+        // console.log(parent)
+        var foo = element;
+        var list = document.getElementById("list");
+        list.className = "list paper paper-list "
+
+        document.getElementById("alltext").value += " " + foo.innerText.replace(/[\n\r]+|[\s]{2,}/g, ' ') || " " + foo.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ')
+    }
+
+    function copy() {
+        if (false == document.execCommand('copy')) {
+            var btn = document.getElementById("node-btn");
+            btn.className += " hidden"
+            var btnJs = document.getElementById("js-btn");
+            btnJs.className = "form-btn js-btn "
+        } else {
+            var copyText = document.getElementById("alltext");
+            console.log(copyText)
+            copyText.select();
+            document.execCommand("copy");
+            console.log("copy copy")
+        }
     }
 })();
